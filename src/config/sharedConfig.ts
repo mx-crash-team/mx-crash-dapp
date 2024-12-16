8326759deee103f99f6c7ff5b4e69f650596a413
Radu Mojic <radu.mojic@multiversx.com>
2024-12-17 00:53:54 +0200

    wss

--- src/config/sharedConfig.ts
+++ src/config/sharedConfig.ts
@@ -1,7 +1,7 @@
 export const walletConnectV2ProjectId = '9b1a9564f91cb659ffe21b73d5c4e2d8';
 export const contractAddress =
   'erd1qqqqqqqqqqqqqpgqjhaqmlan0j029lgz2dlmyf4hpv2t3c6hep6q6xjv56';
-export const websocketUrl = '192.168.30.197';
+export const websocketUrl = 'wss://...';
 export const SC_GAS_LIMIT = 40_000_000;
 export const DIGITS = 2;
 export const MAX_DISPLAY_ZERO_DECIMALS = 5;
