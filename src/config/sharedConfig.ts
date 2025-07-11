a01db83a10421693bfd7d459cebac49f37dfb4b0
Andrei Baltariu <andreibaltariu1304@gmail.com>
2025-07-11 12:24:34 +0300

    Modified sc address

--- src/config/sharedConfig.ts
+++ src/config/sharedConfig.ts
@@ -1,6 +1,6 @@
 export const walletConnectV2ProjectId = '9b1a9564f91cb659ffe21b73d5c4e2d8';
 export const contractAddress =
-  'erd1qqqqqqqqqqqqqpgqdey64re8vkszfzzx9mvhc0x0gkul7yljeyvsq4k7lj';
+  'erd1qqqqqqqqqqqqqpgqs0rnd9mqzkfrvpx6y76x9zkr8m36f38xeyvs60nlcw';
 export const websocketUrl = 'https://lfx6frfj-3099.euw.devtunnels.ms/';
 export const SC_GAS_LIMIT = 40_000_000;
 export const DIGITS = 2;
