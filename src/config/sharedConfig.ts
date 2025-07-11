6ee3d52f6bea7885b1d7f34ee6101389e0641c70
Andrei Baltariu <andreibaltariu1304@gmail.com>
2025-07-11 13:18:59 +0300

    Modified sc address

--- src/config/sharedConfig.ts
+++ src/config/sharedConfig.ts
@@ -1,6 +1,6 @@
 export const walletConnectV2ProjectId = '9b1a9564f91cb659ffe21b73d5c4e2d8';
 export const contractAddress =
-  'erd1qqqqqqqqqqqqqpgqs0rnd9mqzkfrvpx6y76x9zkr8m36f38xeyvs60nlcw';
+  'erd1qqqqqqqqqqqqqpgq9fnavdl92tfem033fsnf5p4ka9vuzrjlwzps5yaagr';
 export const websocketUrl = 'ws://localhost:3099';
 export const SC_GAS_LIMIT = 40_000_000;
 export const DIGITS = 2;
