c486760ea8779611ea03b4d98cd3302df29279b5
Andrei Baltariu <andreibaltariu1304@gmail.com>
2025-07-15 11:10:14 +0300

    Moved animation on top

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,11 +5,13 @@ export const Home = () => {
     <div className='home d-flex justify-content-center flex-fill'>
       <div className='container d-flex flex-column'>
         <div className='row mb-spacer3'>
-          <div className='col-12 col-lg-4 order-lg-last'>
+          <div className='col-12'>
+            <Chart />
+          </div>
+          <div className='col-12 mt-3'>
             <Interface />
           </div>
-          <div className='col-12 col-lg-8 d-flex flex-column gap-4 mt-3 mt-lg-0'>
-            <Chart />
+          <div className='col-12 mt-3'>
             <div className='row g-3'>
               <div className='col-6'>
                 <Table />
